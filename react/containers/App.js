8d7d7a8a83961262ee95814797c26792168054b6
hdriqi <riqi.asdf@gmail.com>
2017-04-21 11:24:30 +0700

    modelColor can be changed

--- react/containers/App.js
+++ react/containers/App.js
@@ -2,12 +2,30 @@ import React, {Component} from 'react';
 import STLViewer from '../../src/STLViewer';
 
 class App extends Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			color: '#FF0000'
+		}
+
+		this.clickBlue = this.clickBlue.bind(this)
+		this.clickRed = this.clickRed.bind(this)
+	}
+
+	clickBlue() {
+		this.setState({color: '#0000FF'})
+	}
+
+	clickRed() {
+		this.setState({color: '#FF0000'})	
+	}
 
   render(){
     return (
       <div>
-        <STLViewer url="dist/static/crazy-thing.stl" />
-        <STLViewer url="dist/static/bottle.stl" rotate={false} />
+        <STLViewer url="dist/static/crazy-thing.stl" modelColor={this.state.color} />
+        <button onClick={this.clickRed} >red</button>
+        <button onClick={this.clickBlue} >blue</button>
       </div>
     );
   }
